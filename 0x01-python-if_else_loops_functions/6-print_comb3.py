079d6ebeb1a08a3bd21e779991e4858d58e3017c
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-11 07:59:26 +0300

    format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(10):
     for j in range(10):
         if i < j and (i+j != 17):
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i, j), end="")
 else:
     print(f"{89}")
