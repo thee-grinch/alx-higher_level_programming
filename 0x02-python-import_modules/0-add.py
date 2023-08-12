00d467ec6f2f4602dbfad876b65b48e8c3b4980b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-12 08:39:10 +0300

    tried fixing

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-def main():
-
+if __name = "__main__":
     a = 1
     b = 2
 
     from add_0 import add
 
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-if __name__ = "__main__":
-    main()
