adf87865984bb2255a9cc2262daa3c69bbdacd3a
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-15 05:25:04 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    new_string = ""
     for char in my_string:
-        if char == "c":
-            my_string.remove("c")
-        if char == "C":
-            my_string.remove("C")
-            
-    return my_string
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
