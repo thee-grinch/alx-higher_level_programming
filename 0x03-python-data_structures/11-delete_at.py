c2a347f291e76418c98b4cdd58782e3e6f9bab92
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-15 21:39:04 +0300

    12-switch.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -7,6 +7,5 @@ def delete_at(my_list=[], idx=0):
     for i in range(len(my_list)):
         if i != idx:
             new_list.append(my_list[i])
-    my_list.clear()
-    my_list.extend(new_list)
+    my_list[:] = new_list
     return new_list
