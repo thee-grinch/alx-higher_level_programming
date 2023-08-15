0ad0a641fb05a12e863a582e00f781106af64ad0
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-15 05:09:19 +0300

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,9 @@
 
 def no_c(my_string):
     for char in my_string:
-        if char == "c" or char == "C":
-            char = ""
+        if char == "c":
+            my_string.remove("c")
+        if char == "C":
+            my_string.remove("C")
+            
     return my_string
