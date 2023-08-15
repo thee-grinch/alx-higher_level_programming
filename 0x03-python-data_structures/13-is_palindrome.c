06ac15593265afb50ef71c1ecfa9dd5c8f8996ce
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-15 23:02:49 +0300

    INTERVIEW PREPARATION

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks whether a string is a palindrome
+ * @head: pointer to the head of the linked list
+ * Return: returns 1 if true or otherwise 0
+ */
+int is_palindrome(listint_t **head)
+{
+	int len, i, j, *datas;
+	listint_t *buffer;
+
+	if (!(*head))
+		return (1);
+	len = 0;
+	buffer = *head;
+	while (buffer->next)
+	{
+		len++;
+		buffer = buffer->next;
+	}
+	datas = malloc(len * sizeof(int));
+	if (datas == NULL)
+		return (0);
+	buffer = *head;
+	for (i = 0; buffer != NULL; i++)
+	{
+		datas[i] = buffer->n;
+		buffer = buffer->next;
+	}
+	i--;
+	for (j = 0; j <= (len / 2); j++)
+	{
+		if (datas[j] == datas[i])
+			i--;
+		else
+			return (0);
+	}
+	free(datas);
+	return (1);
+}
