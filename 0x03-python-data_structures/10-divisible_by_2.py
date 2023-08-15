f24b91ba3739bbbb21b43413233cccac09e87b4a
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-15 20:43:35 +0300

    ./10-main.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-def divisibe_by_2(my_list=[]):
-    new_list = ""
+def divisible_by_2(my_list=[]):
+    new_list = []
 
     for num in my_list:
         if num % 2 == 0:
-            new_list += True
+            new_list.append(True)
         else:
-            new_list += False
+            new_list.append(False)
     return new_list
