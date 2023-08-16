d4f8662a17cb7c5c454dafe94a46a5c98e9d756e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-17 02:50:49 +0300

    removed sorted

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    keys = sorted(a_dictionary.keys())
+    keys = list(a_dictionary.keys())
+    keys.sort()
     for key in keys:
         print("{} : {}".format(key, a_dictionary[key]))
