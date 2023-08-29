09287b5bb6bc4a687b08554fb4613d955d2068a2
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-29 21:55:23 +0300

    remolved the index exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,7 +12,7 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             idx += 1
             pass
-        finally:
-            pass
+        except IndexError:
+            break
     print()
     return count
