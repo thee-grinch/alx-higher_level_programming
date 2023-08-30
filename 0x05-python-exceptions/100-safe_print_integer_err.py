885456203ad8ea8ef239ae01d1a19fad915b4bf6
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-08-30 16:55:50 +0300

    task 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as ex:
+    except (ValueError, TypeError, OverflowError) as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
         return False
