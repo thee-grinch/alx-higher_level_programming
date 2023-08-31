d80f057ccfc637d97089adf67dc73025f302ac72
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-09-01 02:10:46 +0300

    task 1 correction

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -6,7 +6,7 @@ class Square:
     """this is a square class. """
     def __init__(self, size):
         """This is a docstring for the init method.
-        
+
         Args:
             size: The length of the square
         """
