df6ea2c2c77458bfcb60dbfb70af0e16a8c23020
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-09-01 03:01:03 +0300

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A square module"""
+
+
+class Square:
+    """this is a square class. """
+    def __init__(self, size=0):
+        """This is a docstring for the init method.
+
+        Args:
+            size: The length of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """this function computes area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieves a property"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value=0):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
