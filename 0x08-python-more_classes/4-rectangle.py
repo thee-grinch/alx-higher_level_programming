84ab50b990d03b3e4ee29786baae078535915135
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-02 22:49:00 +0300

    commit_message

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -65,3 +65,7 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
+    def __repr__(self):
+        """prints the string representation os a class"""
+        return f"Rectangle({self.__width}, {self.__height})"
