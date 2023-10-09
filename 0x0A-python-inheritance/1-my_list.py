c0da1af417d87ecb8472d8a71d98adef4f1ff6dd
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-09 21:59:01 +0300

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python3
+"""this instance derives a class my list"""
+
+
 class MyList(list):
     """is a class under my list"""
-
-
     def print_sorted(self):
         """prints a list which is sorted"""
         l1 = self.copy()
