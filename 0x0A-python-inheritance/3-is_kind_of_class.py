b27c5bbf9c90166182593345ba222e9ab941d8d9
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-09 22:36:01 +0300

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""checks whether a class is an instance"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks whether an object is of type"""
+    return isinstance(obj, a_class)
