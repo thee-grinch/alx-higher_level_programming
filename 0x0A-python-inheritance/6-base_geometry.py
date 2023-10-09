b82de48c54aa5fded8ad1247e2714b57b2de1c39
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-09 23:26:12 +0300

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this is a module with a class"""
+
+
+class BaseGeometry():
+    """this is a class"""
+    def area(Self):
+        """this is a method"""
+        raise Exception("area() is not implemented")
