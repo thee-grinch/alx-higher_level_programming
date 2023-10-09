a42518ac1a30102a3ad6f5410ee1741a08c4e705
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-09 22:04:33 +0300

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,6 +6,4 @@ class MyList(list):
     """is a class under my list"""
     def print_sorted(self):
         """prints a list which is sorted"""
-        l1 = self.copy()
-        l1.sort()
-        print(l1)
+        print(sorted(self))
