79a2862adf9aa2092e507ec1248a084dc8c97a5b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-10 22:13:44 +0300

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,3 +6,5 @@ def write_file(filename="", text=""):
     """function to write to a function"""
     with open(filename, 'w', encoding="utf-8") as file:
         file.write(text)
+    written = len(text)
+    return written
