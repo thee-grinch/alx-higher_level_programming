9d1baaf269350436bee8297035e94abb90ae5584
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-10 22:33:54 +0300

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""this module defines a function that 
+"""this module defines a function that
         appends to a file"""
+
+
 def append_write(filename="", text=""):
     """this function appends to a file"""
     try:
