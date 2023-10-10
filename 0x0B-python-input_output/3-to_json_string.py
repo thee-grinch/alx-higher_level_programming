43c3654fdac9b93bfa974d9c03babe6974903392
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-10 23:00:09 +0300

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module defines a function"""
+
+
+import json
+def to_json_string(my_obj):
+    """this function returns the json representation of an object"""
+    return json.dumps(my_object)
