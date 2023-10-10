c7e0f79a6b8da76a2a98a2a41b4c1c5f0bd3bb39
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-10 22:45:13 +0300

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,6 +5,5 @@
 
 def append_write(filename="", text=""):
     """this function appends to a file"""
-    try:
-        with open(filename, "a", encoding="utf-8") as file:
-            return file.write(text)
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
