6ce80480881c2230cb916bd6402dfa1fac270f29
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-10 21:53:29 +0300

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
 
     with open(filename, 'r', encoding="utf-8") as file:
         for line in file:
-            print({}.format(line))
+            print("{}".format(line), end="")
