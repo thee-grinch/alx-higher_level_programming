aa59c4e8c70b908327eaa69c6aa7ac7c7fd367fb
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-10 22:06:47 +0300

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this module defines a function that writes to a file"""
+
+
+def write_file(filename="", text=""):
+    """function to write to a function"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(text)
