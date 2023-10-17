a635c3313ed4b7e961c5de149507226a559d0006
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-17 16:24:06 +0300

    task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base():
     @staticmethod
     def to_json_string(list_dictionaries):
         """this returns json representation of a dictionary"""
-        if len(list_dictionaries) == 0 or list_dictionaries is None:
+        if list_dictionaries == [] or list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
