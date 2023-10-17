74da1831b8d75ab9ccce947a7932708c93a6452d
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-10-17 20:56:38 +0300

    task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base():
     @staticmethod
     def from_json_string(json_string):
         """converts a dictionary from a json string"""
-        if json_string is None:
+        if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
 
