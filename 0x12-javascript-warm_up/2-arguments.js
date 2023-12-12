f940e0a07911a58cc987e66dd947a2232860ff07
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-12 20:19:57 +0300

    task 2 fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 
-if (process.argv.length === 1)
-{
-	console.log('No argument');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Argument found');
 }
-else if (process.argv.length === 2)
-{
-	console.log('Argument found');
-}
-else
-	console.log('Argument found');
