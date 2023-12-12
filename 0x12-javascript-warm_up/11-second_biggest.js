26a5857f2e9b4532ce8c5b227c0a4cbd5f2082e4
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-12 22:59:46 +0300

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,10 @@ let largest = 0;
 let second = 0;
 
 for (let i = 0; i < process.argv.length; i++) {
-	let myInt = Number(process.argv[i]);
-	if (myInt > largest) {
-		second = largest;
-		largest = myInt;
-	}
+  const myInt = Number(process.argv[i]);
+  if (myInt > largest) {
+    second = largest;
+    largest = myInt;
+  }
 }
 console.log(second);
