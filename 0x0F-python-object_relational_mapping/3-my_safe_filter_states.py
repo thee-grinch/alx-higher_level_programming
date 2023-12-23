a0bda2ba3f1e7d2d92a102c5bc32c1cc593d26ef
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-23 17:11:29 +0300

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" this module uses mysqldb  and filters """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    us = sys.argv[1]
+    pas = sys.argv[2]
+    data = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=us, passwd=pas, db=data)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
