6a06c8e56411032dc490a6ae4f31abcb0671482b
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-23 10:28:44 +0300

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this module uses mysqldb """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    us = sys.argv[1]
+    pas = sys.argv[2]
+    data = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306, user=us, passwd=pas, db=data)
+    cursor = db.cursor()
+    sql_query = "SELECT * FROM states WHERE name LIKE "N%""
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
