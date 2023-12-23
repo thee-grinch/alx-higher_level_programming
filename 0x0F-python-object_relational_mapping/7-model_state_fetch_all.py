7d141fed6a71c574afb960747b63234289a2a7c3
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-23 23:17:33 +0300

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""This model uses  an engine to query data"""
+""" This model uses  an engine to query data
+"""
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import base, State
+
 
 if __name__ == "__main__":
 
