13e5091c3df00a72ea125ae4bb7ace21a1ec606c
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-23 23:28:49 +0300

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,10 +9,8 @@ from model_state import Base, State
 
 if __name__ == "__main__":
 
-    DATABASE_URL = f"""mysql+mysqldb://{argv[1]}
-    :{argv[2]}@localhost:3306/{argv[3]}"""
-
-    engine = create_engine(DATABASE_URL, echo=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
