7c8898c71e2d75552c00344b2c12b8be2eb3da6e
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-23 10:19:31 +0300

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     pas = sys.argv[2]
     data = sys.argv[3]
 
-    db = MySQLdb.connect(host="127.0.0.1", port=3306, user=us, passwd=pas, db=data)
+    db = MySQLdb.connect(host="localhost", port=3306, user=us, passwd=pas, db=data)
 
     cursor = db.cursor()
 
