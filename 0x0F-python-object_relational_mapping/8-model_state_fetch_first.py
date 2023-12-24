ec4200247c184bad9cc7757cbf9845b8d70d0c86
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-24 19:16:44 +0300

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).limit(1)
+    state = session.query(State).first()
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
