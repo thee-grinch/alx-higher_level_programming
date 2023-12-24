3f3ca25af11ff2ea940a40db53962d8557b6df76
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-24 22:51:18 +0300

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,10 +14,10 @@ if __name__ == "__main__":
 
     matched = argv[4]
     state = session.query(State).filter(
-        State.name==matched).first()
+        State.name == matched).first()
     if state:
         print('{}'.format(state.id))
     else:
-        print("Not Found")
+        print("Not found")
 
     session.close()
