adca585bf848affd6682072de9852bb8aeb0766d
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-25 12:42:22 +0300

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-""" This module displays all list
+""" prints the State object with the name passed as argument from the database
 """
-from sys import argv
-from model_state import State, Base
+import sys
+from model_state import Base, State
 from model_city import City
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    instances = session.query(State.name, City.id, City.name).filter(State.id == City.states_id).order_by(City.id).all()
-    for instance in instances:
-        print('{}: ({}) {}'.format(instance[0], instance[1], instance[2]))
-    session.close()
-
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-this model creates a cities schema
+Contains the class definition of a City
 """
-from sqlalchemy import Colunm, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
     """
-    this is a schema class
+    Class that defines each city
     """
     __tablename__ = 'cities'
-
-    id = Column(Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
