92b9b0ac5d6f894be263300eb9bcb8af66d7d5ce
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-25 12:31:15 +0300

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,7 +3,7 @@
 from sqlalchemy import Colunm, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-Base = declarative_base()
+from model_state import Base
 
 class City(Base):
     """ this is a schema class"""
