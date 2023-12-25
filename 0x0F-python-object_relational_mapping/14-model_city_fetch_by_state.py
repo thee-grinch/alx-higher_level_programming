e829f95f53d2ecca400c35be94ebf0c95e4df9b0
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2023-12-25 12:39:20 +0300

    task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""This module displays all list"""
+""" This module displays all list
+"""
+from sys import argv
 from model_state import State, Base
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
+
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    Base.metadata.create_all(engine)
 
     instances = session.query(State.name, City.id, City.name).filter(State.id == City.states_id).order_by(City.id).all()
     for instance in instances:
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-""" this model creates a cities schema """
+"""
+this model creates a cities schema
+"""
 from sqlalchemy import Colunm, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
-    """ this is a schema class"""
+    """
+    this is a schema class
+    """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, nullable=False, ForeignKey('states.id'))
