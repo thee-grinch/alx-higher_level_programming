240afdc24920a6c59193b7953314f1e293ec5dd5
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-01-08 01:07:25 +0300

    task 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,6 +8,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     data = {'email': sys.argv[2]}
-    encoded = urllib.parse.urlencode(data).encode('utf-8')
+    encoded = urllib.parse.urlencode(data).encode('ascii')
     with urllib.request.urlopen(encoded, data=encoded) as response:
         print(response.read().decode('utf-8'))
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This message prints an error"""
+
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
