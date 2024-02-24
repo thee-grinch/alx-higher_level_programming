cf119bf52004cf4fbe6535c5b9de3c18e7c7bbae
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-24 12:00:32 +0300

    extra tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const myDict = require('./101-data').dict;
+const newDict = {};
+for (const key in myDict) {
+  if (newDict[myDict[key]] === undefined) {
+    newDict[myDict[key]] = [];
+  }
+  newDict[myDict[key]].push(key);
+}
