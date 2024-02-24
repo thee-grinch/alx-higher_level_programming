9bcff6ff03ac3b0bf674e2c1a73d9be3bc641eec
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-24 09:44:01 +0300

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -6,5 +6,17 @@ class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    let i;
+    let j;
+    let row = '';
+    for (i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (j = 0; j < this.height; j++) {
+      console.log(row);
+    }
+  }
 }
 module.exports = Rectangle;
