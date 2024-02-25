4c84708c99d6adcb2185ef6d7d40643a71c4fcf0
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-25 20:03:50 +0300

    task 0

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const characterId = 18;
+
+request(apiUrl, function(error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+
+    films.forEach(function(film) {
+      const characters = film.characters;
+      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+
+    console.log(`Number of movies where "Wedge Antilles" is present: ${count}`);
+  }
+});
