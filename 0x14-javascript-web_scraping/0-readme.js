5941aaa96ad7a39c5fb9bdc791a018ecee135c52
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-25 19:04:33 +0300

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,20 +1,9 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-// Check if the file path argument is provided
-if (process.argv.length < 3) {
-    console.error('Usage: node read_file.js <file_path>');
-    process.exit(1);
-}
-
-const filePath = process.argv[2];
-
-// Read the content of the file
-fs.readFile(filePath, 'utf8', function(err, data) {
-    if (err) {
-        console.error(err); // Print the error object if an error occurred
-        return;
-    }
-    console.log(data); // Print the content of the file
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
