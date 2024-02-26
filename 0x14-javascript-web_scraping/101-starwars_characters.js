a7fb90692c18c35b62e6a4b6785a1120456e164f
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-26 06:20:30 +0300

    all tasks

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,21 +6,21 @@ const movieId = process.argv[2];
 const url = `https://swapi.dev/api/films/${movieId}/`;
 
 request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    const film = JSON.parse(body);
-    const characters = film.characters;
+    if (error) {
+        console.error('Error:', error);
+    } else {
+        const film = JSON.parse(body);
+        const characters = film.characters;
 
-    characters.forEach((characterUrl) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          console.error('Error:', error);
-        } else {
-          const character = JSON.parse(body);
-          console.log(character.name);
-        }
-      });
-    });
-  }
+        characters.forEach((characterUrl) => {
+            request(characterUrl, (error, response, body) => {
+                if (error) {
+                    console.error('Error:', error);
+                } else {
+                    const character = JSON.parse(body);
+                    console.log(character.name);
+                }
+            });
+        });
+    }
 });
