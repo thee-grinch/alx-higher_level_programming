e252326376e3fadf27461441cc2d0bee669a8ac9
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-26 05:56:49 +0300

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  }
+});
