b1bbf1526e3ffc033b36f0d06e5fb359adfdc341
Mordecai Muvandi <mordecaimuvandi16@gmail.com>
2024-02-27 07:01:32 +0300

    task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 #displays only body of 200 requests
-curl -s -f -l $1
-#!/bin/bash
-# displays only body of 200 requests
-curl -s -f -l $1 | grep -v "HTTP/" | sed -n '/^$/,$p'
-#!/bin/bash
-# displays only body of 200 requests and counts redirects
-curl -s -f -l -o /dev/null -w 'with %{num_redirects} redirections' $1
+curl -sL $1
